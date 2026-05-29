49daa7db4dc997dc7fa2c23dd3c1af59930d2eae
Ivan <ivan.scillone@gmail.com>
2026-05-29 14:21:36 +0200

    Inizio implem. upload

--- index.php
+++ index.php
@@ -1,25 +1,35 @@
 <?php
 require_once './vendor/autoload.php';
-
 $dbg=new MyClasses\Debug();
 $smarty=new Smarty\Smarty();
 $smarty->setTemplateDir('./templates');
 
+$dbg->log($_REQUEST);
 $smarty->assign(
     'serverURL',(($_SERVER['HTTPS'] ?? 'off') == 'on' ? 'https' : 'http') . 
     '://' . $_SERVER['HTTP_HOST']
 );
-$smarty->assign('noSi',['no','sì']);
-if (isset($_REQUEST['action'])) {
+//$smarty->assign('noSi',['no','sì']);
+if (isset($_REQUEST['goTo'])) {
+    $smarty->display($_REQUEST['goTo'].'.html');
+} elseif (isset($_REQUEST['action'])) {
     switch ($_REQUEST['action']) {
-        case 'download':
-            $dbg->log('download');
-            $smarty->display('download.html');
-            break;
         case 'insert':
-            $smarty->assign('feedback',json_encode($_REQUEST));
+            $f=fopen($_REQUEST['file'],'r');
+            $firstRow=true;
+            while ($row=fgetcsv($f,null,';')) {
+                if ($firstRow) {
+                    $firstRow=false;
+                    continue;
+                }
+                $dbg->log($row);
+            }
+            fclose($f);
             $smarty->display('index.html');
             break;
+        default:
+            $smarty->assign('feedback','<p class="font-red blink-fast">NOT IMPLEMENTED!</p>');
+            $smarty->display('index.html');
     }
 } else {
     $smarty->display('index.html');
