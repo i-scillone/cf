0e04c3cf29e8c161d6d8eb36b57c49be208301cd
Ivan <ivan.scillone@gmail.com>
2026-05-30 14:03:37 +0200

    Importaz. del DB

--- index.php
+++ index.php
@@ -3,7 +3,16 @@ require_once './vendor/autoload.php';
 $dbg=new MyClasses\Debug();
 $smarty=new Smarty\Smarty();
 $smarty->setTemplateDir('./templates');
-
+const INIT_DB=<<<SQL
+CREATE TABLE IF NOT EXISTS comuni (
+    codice TEXT PRIMARY KEY,
+    nome TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS stati (
+    codice TEXT PRIMARY KEY,
+    nome TEXT NOT NULL
+);
+SQL;
 $dbg->log($_REQUEST);
 $smarty->assign(
     'serverURL',(($_SERVER['HTTPS'] ?? 'off') == 'on' ? 'https' : 'http') . 
@@ -14,21 +23,42 @@ if (isset($_REQUEST['goTo'])) {
     $smarty->display($_REQUEST['goTo'].'.html');
 } elseif (isset($_REQUEST['action'])) {
     switch ($_REQUEST['action']) {
-        case 'insert':
-            $f=fopen($_REQUEST['file'],'r');
-            $firstRow=true;
-            while ($row=fgetcsv($f,null,';')) {
-                if ($firstRow) {
-                    $firstRow=false;
-                    continue;
+        case 'upload':
+            try {
+                $db=new MyClasses\DB('sqlite:'.__DIR__.'/codici.sqlite');
+                $db->exec(INIT_DB);
+                $f=fopen($_REQUEST['file'],'r');
+                $table=pathinfo($_REQUEST['file'],PATHINFO_FILENAME);
+                $db->exec('DELETE FROM '.$table);
+                $ins=$db->prepare("INSERT INTO {$table} VALUES(?,?)");
+                $firstRow=true;
+                while ($row=fgetcsv($f,null,';')) {
+                    if ($firstRow) {
+                        $firstRow=false;
+                        continue;
+                    }
+                    if ($table=='comuni') {
+                        $ins->bindValue(1,$row[0],PDO::PARAM_STR);
+                        $ins->bindValue(2,$row[2],PDO::PARAM_STR);
+                    } else {
+                        $ins->bindValue(1,$row[0],PDO::PARAM_STR);
+                        $ins->bindValue(2,$row[1],PDO::PARAM_STR);
+                    }
+                    $ins->execute();
                 }
-                $dbg->log($row);
+                fclose($f);
+                $db=null;
+                $smarty->assign('feedback',"Tabella $table importata");
+            } catch (Exception $e) {
+                $smarty->assign(
+                    'error',
+                    "Errore alla riga {$e->getLine()}: «{$e->getMessage()}»"
+                );
             }
-            fclose($f);
-            $smarty->display('index.html');
+            $smarty->display('download.html');
             break;
         default:
-            $smarty->assign('feedback','<p class="font-red blink-fast">NOT IMPLEMENTED!</p>');
+            $smarty->assign('error','NOT IMPLEMENTED!');
             $smarty->display('index.html');
     }
 } else {
